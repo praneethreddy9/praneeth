00df9351603e74d9c7b8ad2ec88692e0b757891d
ADMIN <ADMIN@PLACEMENT-LAB-105>
2020-02-17 15:29:52 +0530

    dadims

---
+++ DrinkAndDelightInventoryManagementSystem/src/main/java/com/capg/sprint1/dadims/model/PlaceOrder.java
@@ -0,0 +1,20 @@
+package com.capg.sprint1.dadims.model;
+
+import java.util.Date;
+
+public class PlaceOrder {
+
+	public static void main(String[] args) {
+		
+		String rawMaterialName;
+		String supplierId;
+		String warehouseId;
+		long quantityVaue;
+		long quantityUnit;
+		float pricePerUnit;
+		Date expectedDate;
+
+	}
+
+
+}
